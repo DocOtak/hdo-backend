cd82938794cb76495a6d259255ba084792ad7e6c
Andrew Barna <abarna@gmail.com>
2024-09-27 14:58:01 -0600

    continue porting cruise schema

--- schemas.py
+++ schemas.py
@@ -1,7 +1,9 @@
 import json
 from enum import Enum
+from datetime import datetime, date
+from typing import Literal
 
-from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from pydantic import BaseModel, ConfigDict, Field, EmailStr, constr
 from pydantic.json_schema import SkipJsonSchema
 
 # https://skaaptjop.medium.com/how-i-use-pydantic-unrequired-fields-so-that-the-schema-works-0010d8758072
@@ -34,6 +36,29 @@ class Participant(BaseModel):
     institution: str = ""
     email: str = ""
 
+class Note(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    name: str
+    data_type: str
+    action: str
+    summary: str
+    date: datetime
+    body: list[str]
+
+class Collections(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    woce_lines: set[constr(min_length=1, pattern=r"^\S")]
+    programs: set[constr(min_length=1, pattern=r"^\S")]
+    oceans: set[constr(min_length=1, pattern=r"^\S")]
+    groups: set[constr(min_length=1, pattern=r"^\S")]
+
+class Sites(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    microstructure_ucsd_edu: dict | SkipJsonSchema[None] = Field(None, json_schema_extra=pop_default_from_schema, alias="microstructure.ucsd.edu")
+    dimes_ucsd_edu: dict | SkipJsonSchema[None] = Field(None, json_schema_extra=pop_default_from_schema, alias="dimes.ucsd.edu")
 
 
 class Cruise(BaseModel):
@@ -43,10 +68,17 @@ class Cruise(BaseModel):
     ship: str
     participants: list[Participant]
     files: set[int]
-    cf_robots: set[CFRobotEnum] = set()
+    notes: list[Note]
+    collections: Collections
 
+    startDate: date | Literal[""]
+    startEdate: date | Literal[""]
+
+    # optional things here
+    cf_robots: set[CFRobotEnum] = set()
     description: str | SkipJsonSchema[None] = Field(None, json_schema_extra=pop_default_from_schema)
     license: License | SkipJsonSchema[None] = Field(None, json_schema_extra=pop_default_from_schema)
+    sites: Sites | SkipJsonSchema[None] = Field(None, json_schema_extra=pop_default_from_schema)
 
 main_model_schema = Cruise.model_json_schema()
 print(json.dumps(main_model_schema, indent=2))
